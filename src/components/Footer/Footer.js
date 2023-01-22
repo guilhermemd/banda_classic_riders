2ac5233eb6f5525bfae69b1cd88b4e68d7167a5f
guilherme <guilherme.md@gmail.com>
2023-01-22 18:13:47 -0300

    feat: add links on social media and whatsapp

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,17 +16,46 @@ const Footer = () => {
         <div className="footer__contact">
           <p>Contato:</p>
           <div className="footer__contact__wrapper">
-            <img src={whatsapp} alt="whatsapp" className="whatsapp" />
+            <a
+              href="https://api.whatsapp.com/send?phone=5551989476508&text=Texto%20aqui"
+              target="_blank"
+            >
+              <img src={whatsapp} alt="whatsapp" className="whatsapp" />
+            </a>
             <p>(51) 98947-6508</p>
           </div>
         </div>
         <div className="footer__social">
           <p>Redes Sociais:</p>
           <div className="footer__social__wrapper">
-            <img src={instagram} alt="instagram" className="instagram" />
-            <img src={facebook} alt="facebook" className="facebook" />
-            <img src={youtube} alt="youtube" className="youtube" />
-            <img src={spotify} alt="spotify" className="spotify" />
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/bandaclassicriders/"
+            >
+              <img src={instagram} alt="instagram" className="instagram" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/CLASSICRIDERS/"
+            >
+              <img src={facebook} alt="facebook" className="facebook" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.youtube.com/@classicriders7066/videos"
+            >
+              <img src={youtube} alt="youtube" className="youtube" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://open.spotify.com/artist/4H0xqk7xLM2z3k4HFKmzH1"
+            >
+              <img src={spotify} alt="spotify" className="spotify" />
+            </a>
           </div>
         </div>
       </div>
